bcfaa792034416ea6a56f626c3357054246dc5fb
meshach5667 <meshachzakumi@gmail.com>
2024-09-20 10:01:26 +0100

    redeploy

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,14 +1,14 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
-	anvil,
-	arbitrum,
-	arbitrumSepolia,
-	base,
-	hardhat,
-	mainnet,
-	optimism,
-	polygon,
-	sepolia,
+    anvil,
+    arbitrum,
+    arbitrumSepolia,
+    base,
+    hardhat,
+    mainnet,
+    optimism,
+    polygon,
+    sepolia,
 } from "wagmi/chains";
 import { cookieStorage, createStorage } from "wagmi";
 import { defineChain } from "viem";
@@ -16,40 +16,42 @@ import { defineChain } from "viem";
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string;
 
 const localhost = defineChain({
-	id: 31337,
-	name: "Anvil Foundry",
-	nativeCurrency: {
-		name: "Ether",
-		decimals: 10,
-		symbol: "ETH",
-	},
-	rpcUrls: {
-		default: {
-			http: ["http://localhost:8545"],
-		},
-	},
+    id: 31337,
+    name: "Anvil Foundry",
+    nativeCurrency: {
+        name: "Ether",
+        decimals: 10,
+        symbol: "ETH",
+    },
+    rpcUrls: {
+        default: {
+            http: ["http://localhost:8545"],
+        },
+    },
 });
 
 const config = getDefaultConfig({
-	appName: "StackUp Forums",
-	projectId: projectId,
-	storage: createStorage({
-		storage: cookieStorage,
-	}),
-	chains:
-		process.env.NEXT_PUBLIC_ONLY_ANVIL_HARDHAT_TESTNETS === "true"
-			? [anvil, hardhat]
-			: [
-					mainnet,
-					polygon,
-					optimism,
-					arbitrum,
-					base,
-					...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-						? [sepolia, arbitrumSepolia, anvil]
-						: []),
-				],
-	ssr: true,
+    appName: "StackUp Forums",
+    projectId: projectId,
+    storage: createStorage({
+        storage: cookieStorage,
+    }),
+    chains:
+        process.env.NEXT_PUBLIC_ONLY_ANVIL_HARDHAT_TESTNETS === "true"
+            ? [anvil, hardhat]
+            : [
+                    mainnet,
+                    polygon,
+                    optimism,
+                    arbitrum,
+                    base,
+                    sepolia, 
+                                        arbitrumSepolia, 
+                                        anvil
+                          ],
+
+    ssr: true,
 });
+
 
 export default config;
